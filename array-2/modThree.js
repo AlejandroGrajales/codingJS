5966a72205fc5636b3ea998ba26e0cac8bd0ff78
Alejandro Grajales <aleksh06@gmail.com>
2022-05-09 17:02:31 -0500

    resuelto modThree

---
+++ array-2/modThree.js
@@ -0,0 +1,11 @@
+function modThree(nums){
+  for(i = 0; i < nums.length; i++) {
+    if(nums[i] % 2 === 0 && nums[i + 1] % 2 === 0 && nums[i + 2] % 2 === 0) {
+      return true;
+    }
+    if(nums[i] % 2 === 1 && nums[i + 1] % 2 === 1 && nums[i + 2] % 2 === 1) {
+      return true;
+    }
+  }
+  return false;
+}
