426c20a7c7de5942730e2fe863fa0753ba715b1e
Alejandro Grajales <aleksh06@gmail.com>
2022-05-10 20:41:05 -0500

    fizzBuzz resuelto

---
+++ array-2/fizzBuzz.js
@@ -0,0 +1,17 @@
+function fizzBuzz(start, end){
+  let newArr = [];
+  for(i = start; i < end; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+      newArr.push("FizzBuzz");
+    } 
+    else if(i % 3 === 0){
+      newArr.push("Fizz");
+    }
+    else if(i % 5 === 0){
+      newArr.push("Buzz");
+    } else {
+      newArr.push(i.toString());
+    }
+  }
+  return newArr;
+}
